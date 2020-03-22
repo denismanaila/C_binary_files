18ccbc94ac70481fdc343d2ea42fd236dab27925
denismanaila <62503468+denismanaila@users.noreply.github.com>
2020-03-22 13:21:37 +0200

    Create lab5_2.c

---
+++ lab5_2.c
@@ -0,0 +1,5 @@
+/**
+Aplicația 5.2: Scrieți un program care copiază conținutul unui fișier sursă într-un fișier destinație. Numele fișierelor se citesc din linia de comandă. Pentru eficiența copierii, programul va citi câte un bloc de maxim 4096 de octeți, pe care îl va scrie în destinație.
+Exemplu: ./copiere src.dat dst.dat                    -> copiază src.dat în dst.dat
+
+*/
